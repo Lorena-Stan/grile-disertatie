e383a7412a84d29ac8a988190b76504d9043bb0a
Lorena-Stan <curecheriu.ana@gmail.com>
2025-06-18 00:54:09 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,30 @@
+const questionSets = {
+  "aleatoriu_80": [
+    {
+      "intrebare": "pH-ul sângelui variază îl limite foarte restrânse în jurul valorii de:",
+      "variante": [
+        "7.4",
+        "7.3",
+        "7.5"
+      ],
+      "corect": 7,
+      "materie": "Fiziologie"
+    }
+  ],
+  "anatomie_100": [],
+  "farmacologie_100": [],
+  "fiziologie_100": [
+    {
+      "intrebare": "pH-ul sângelui variază îl limite foarte restrânse în jurul valorii de:",
+      "variante": [
+        "7.4",
+        "7.3",
+        "7.5"
+      ],
+      "corect": 7,
+      "materie": "Fiziologie"
+    }
+  ],
+  "patologie_100": [],
+  "bacteriologie_100": []
+};
